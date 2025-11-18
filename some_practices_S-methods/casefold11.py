f066c8fc52ad3f4c9cb6d0f8826f50e26c1e0d65
sir-beluga <followun41@gmail.com>
2025-11-18 16:32:51 +0545

    making lowercase very seriously

---
+++ some_practices_S-methods/casefold11.py
@@ -0,0 +1,10 @@
+# .casefold()
+
+# This dude is like .lower() on steroids.
+
+# It handles international characters, symbols, accented letters, and weird unicode stuff.
+
+# Basically used when u wanna compare text super fairly (like German ß → "ss").
+
+enter=input("enter username you want to set :")
+print(enter.casefold())
