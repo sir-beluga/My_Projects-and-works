3265195db3d7fdaf2f0a33ef405a76343252f9b2
sir-beluga <followun41@gmail.com>
2025-11-18 16:43:18 +0545

    to count amount of character

---
+++ some_practices_S-methods/count0.py
@@ -0,0 +1,4 @@
+email=input(" enter your email :")
+search=input("enter the letter you wanna count on that string")
+rt=email.count(search)
+print(f"there are {rt} numbers of {search} in your string")
