e5be0890136967128e868cb4556286aeab9ff47d
sir-beluga <followun41@gmail.com>
2025-11-21 00:06:06 +0545

    done with encode

---
+++ some_practices_S-methods/encode01.py
@@ -0,0 +1,6 @@
+name="kushal"
+x=name.encode("utf-8")
+# encode() basically yeets my string into byte-language so the computer can vibe with it
+# even if it looks the same, it's NOT a normal string anymore (it's bytes, bro)
+print(x)
+print(type(x))
